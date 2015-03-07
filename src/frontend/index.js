4571ee4b60654b62744b014a617fa72515cc22c7
Marek Bardoński <mb336912@students.mimuw.edu.pl>
2015-03-07 22:18:16 +0100

    Main HTML page.

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,16 +1,36 @@
 requirejs.config({
     baseUrl: '.',
+    shim : {
+        "bootstrap" : { "deps" :['jquery'] }
+    },
+
     paths: {
         lib: '../bower_components',
-        module: '../backend'
+        module: '../backend',
+	jquery: "bower_components/jquery/dist/jquery.min",
+	bootstrap: "bower_components/bootstrap/dist/js/bootstrap.min",
+	cm: "bower_components/codemirror",
+	htmlmixed: "bower_components/codemirror/mode/htmlmixed/htmlmixed"
     }
 });
 
 var backend;
 
 function initBackend() {
-    require(["backend/index"], function(be) {
+    require(["jquery", "bootstrap", "backend/index"], function(be) {
         backend = be;
+    });
+
+    require(["cm/lib/codemirror",
+	     "cm/mode/javascript/javascript",
+	     "cm/addon/selection/active-line",
+	     "cm/addon/edit/matchbrackets"], function(CodeMirror) {
+        CodeMirror.fromTextArea(document.getElementById("code"), {
+            lineNumbers: true,
+	    theme: "solarized light",
+	    styleActiveLine: true,
+	    matchBrackets: true,
+        });
     });
 }
 
