d92c22940912fcf008a455f1bd482a6475cb474f
Adrian Rudolf Kral <adriankral@gmail.com>
2015-05-11 21:05:49 +0200

    Add STEP instruction

---
+++ src/backend/executor/StepInstr.js
@@ -0,0 +1,17 @@
+define(function() {
+
+    function StepInstr(num) {
+	// STEP
+    }
+
+    StepInstr.prototype.invoke = function invoke(context, process) {
+	// STEP
+    };
+
+    StepInstr.prototype.toString = function toString() {
+        return "STEP";
+    };
+
+    return StepInstr;
+    
+});
