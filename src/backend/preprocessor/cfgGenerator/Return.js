03a09742d4c28db0553d5f1ce495e9e92568eb99
Adrian Rudolf Kral <adriankral@gmail.com>
2015-05-21 04:17:41 +0200

    Fix 'return' sole statement

--- src/backend/preprocessor/cfgGenerator/Return.js
+++ src/backend/preprocessor/cfgGenerator/Return.js
@@ -6,30 +6,48 @@ define([
     var cfgGenerator;
     
     function Return(paramNode) {
-	var expr = cfgGenerator(paramNode.rexpression);
+	var result;
 	var return_tvalue = paramNode.return_tvalue;
-	CfgHelper.toValOrPtr(expr);
-
-	if (return_tvalue.type === 'concrete_type') {
-	    if (expr.tvalue.type !== return_tvalue.name) {
-		throw new Errors.TypeMismatch(
-		    return_tvalue.name,
-		    expr.tvalue.type,
-		    'RETURN');
-	    }
-	} else if (expr.tvalue.of.type !== return_tvalue.tvalue.of.name) {
-	    throw new Errors.TypeMismatch(
-		return_tvalue.tvalue.of.name,
-		expr.tvalue.of.type,
-		'RETURN');
-	}
 
 	var returnInstr = new Cfg ({
 	    type: 'RETURN'
 	});
 
-	var result = expr;
-	result.mergeLeft(returnInstr);
+	if (return_tvalue.type === 'concrete_type' && return_tvalue.name === 'void') {
+	    if (paramNode.rexpression) {
+		throw new Errors.TypeMismatch(
+		    'void',
+		    'not void',
+		    'RETURN');
+	    }
+	    result = returnInstr;
+	} else {
+	    if (!paramNode.rexpression) {
+		throw new Errors.TypeMismatch(
+		    return_tvalue.name,
+		    'void',
+		    'RETURN');
+	    }
+		
+	    var expr = cfgGenerator(paramNode.rexpression);
+	    CfgHelper.toValOrPtr(expr);
+
+	    if (return_tvalue.type === 'concrete_type') {
+		if (expr.tvalue.type !== return_tvalue.name) {
+		    throw new Errors.TypeMismatch(
+			return_tvalue.name,
+			expr.tvalue.type,
+			'RETURN');
+		}
+	    } else if (expr.tvalue.of.type !== return_tvalue.tvalue.of.name) {
+		throw new Errors.TypeMismatch(
+		    return_tvalue.tvalue.of.name,
+		    expr.tvalue.of.type,
+		    'RETURN');
+	    }
+	    result = expr;
+	    result.mergeLeft(returnInstr);
+	}
 
 	result.type = null;
 	result.tvalue = null;
